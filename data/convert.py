31f7b6d5c390cdafbb733569f12184a55b9d59bc
bagusbpg <bagusbpg@gmail.com>
2022-11-21 13:17:20 +0700

    add extractXML

--- data/convert.py
+++ data/convert.py
@@ -2,23 +2,30 @@ import cv2
 import os
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit('please input source directory and image extension to convert')
+def convert(imageDir):
+    processedCount = 0
+    for imagePath in os.listdir(imageDir):
+        if imagePath.endswith(extensionBefore):
+            image = cv2.imread(imagePath)
+            fileName = imagePath.split('.')[0]
+            cv2.imwrite(f'1-{fileName}.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            os.remove(f'{imageDir}/{imagePath}')
+            processedCount += 1
+    print(f'successfully converted {processedCount} images')
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.exit('please input source directory and image extension to convert')
 
-imageDir = sys.argv[1]
-if not os.path.exists(imageDir):
-    sys.exit(f'directory {imageDir} does not exist')
-print('image directory:', imageDir)
+    imageDir = sys.argv[1]
+    if not os.path.exists(imageDir):
+        sys.exit(f'directory {imageDir} does not exist')
+    print('image directory:', imageDir)
 
-allowedExtension = ['jfif', 'png']
-extensionBefore = sys.argv[2]
-if extensionBefore not in allowedExtension:
-    sys.exit(f'only jfif and png image extensions are supported')
-print('image extension before converting to jpg:', extensionBefore)
+    allowedExtension = ['jfif', 'png']
+    extensionBefore = sys.argv[2]
+    if extensionBefore not in allowedExtension:
+        sys.exit(f'only jfif and png image extensions are supported')
+    print('image extension before converting to jpg:', extensionBefore)
 
-for imagePath in os.listdir(imageDir):
-    if imagePath.endswith(extensionBefore):
-        image = cv2.imread(imagePath)
-        fileName = imagePath.split('.')[0]
-        cv2.imwrite(f'1-{fileName}.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-        os.remove(f'{imageDir}/{imagePath}')
+    convert(imageDir)
--- data/extractXML.py
+++ data/extractXML.py
@@ -1,32 +1,65 @@
 import os
+import pandas
+import sys
 import xml.etree.ElementTree as xet
 
-coordinates = {}
-brokenXMLs = []
-for imagePath in path:
-    xmlPath = imagePath.replace('jpg', 'xml')
-    if not os.path.exists(xmlPath):
-        coordinates[imagePath] = None
-        brokenXMLs.append(xmlPath)
-        continue
-    parsedXML = xet.parse(xmlPath)
-    root = parsedXML.getroot()
-    coordinate = root.find('object').find('bndbox')
-    xmin = int(coordinate.find('xmin').text)
-    xmax = int(coordinate.find('xmax').text)
-    ymin = int(coordinate.find('ymin').text)
-    ymax = int(coordinate.find('ymax').text)
-    coordinates[imagePath] = [xmin, xmax, ymin, ymax]
+def extractXML(imageDir):
+    coordinates = {'id':[], 'xmin':[], 'xmax':[], 'ymin':[], 'ymax':[]}
+    missingXMLs = []
+    for imagePath in os.listdir(imageDir):
+        xmlPath = f'{imageDir}/{imagePath.replace("jpg", "xml")}'
+        if not os.path.exists(xmlPath):
+            missingXMLs.append(xmlPath)
+            continue
+        parsedXML = xet.parse(xmlPath)
+        root = parsedXML.getroot()
+        coordinate = root.find('object').find('bndbox')
+        xmin = int(coordinate.find('xmin').text)
+        xmax = int(coordinate.find('xmax').text)
+        ymin = int(coordinate.find('ymin').text)
+        ymax = int(coordinate.find('ymax').text)
+        
+        # default image
+        coordinates['id'].append(imagePath)
+        coordinates['xmin'].append(xmin)
+        coordinates['xmax'].append(xmax)
+        coordinates['ymin'].append(ymin)
+        coordinates['ymax'].append(ymax)
+        
+        # image rotated 90-deg clockwise
+        coordinates['id'].append(imagePath.replace('1-', '2-'))
+        coordinates['xmin'].append(600-ymax)
+        coordinates['xmax'].append(600-ymin)
+        coordinates['ymin'].append(xmin)
+        coordinates['ymax'].append(xmax)
+        
+        # image rotated 180-deg clockwise
+        coordinates['id'].append(imagePath.replace('1-', '3-'))
+        coordinates['xmin'].append(600-xmax)
+        coordinates['xmax'].append(600-xmin)
+        coordinates['ymin'].append(600-ymax)
+        coordinates['ymax'].append(600-ymin)
+        
+        # image rotated 270-deg clockwise
+        coordinates['id'].append(imagePath.replace('1-', '4-'))
+        coordinates['xmin'].append(ymin)
+        coordinates['xmax'].append(ymax)
+        coordinates['ymin'].append(600-xmax)
+        coordinates['ymax'].append(600-xmin)
     
-if len(brokenXMLs) == 0:
-    coordinates2 = {}
-    coordinates3 = {}
-    coordinates4 = {}
-    if len(brokenXMLs) == 0:
-        for key, value in coordinates1.items():
-            xmin1, xmax1, ymin1, ymax1 = value
-            coordinates2[key] = 600-ymax1, 600-ymin1, xmin1, xmax1
-            coordinates3[key] = 600-xmax1, 600-xmin1, 600-ymax1, 600-ymin1
-            coordinates4[key] = ymin1, ymax1, 600-xmax1, 600-xmin1
-else:
-    print(f'List of broken XMLs: {brokenXMLs}')
+    return coordinates, missingXMLs
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit('please input source directory')
+    
+    imageDir = sys.argv[1]
+    if not os.path.exists(imageDir):
+        sys.exit(f'directory {imageDir} does not exist')
+    print('image directory:', imageDir)
+
+    coordinates, missingXMLs = extractXML(imageDir)
+
+    if len(missingXMLs) != 0:
+        sys.exit(f'list of missing XMLs: {missingXMLs}')
+    pandas.DataFrame(data=coordinates).set_index('id').to_csv('target.csv')
